6035deb30152ac8f820d590301890b8e0e8e1eef
root <emdeefx@gmail.com>
2022-06-30 08:11:11 -0700

    created file 101.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Does not contain an arg hence 'void'
+ *
+ * Return: returns the value 1 int
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" \- Dora Kopar, 2015\-10\-19\n");
+	return (1);	
+}
