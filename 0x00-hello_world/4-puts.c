dc42018cb9f40c0c2d29961f5e75a55b12d033aa
root <emdeefx@gmail.com>
2022-06-30 04:47:15 -0700

    changed the code syntax in file 4.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+/*The program prints the string when run*/
 int main(void)
 {
-puts("Programming is like building a multilingual pussle\n");
-return (0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
