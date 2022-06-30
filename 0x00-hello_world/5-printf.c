3275722a8e9d02049d2afcbb05e44fd02b244ad3
root <emdeefx@gmail.com>
2022-06-30 05:56:04 -0700

    created file 5.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - Does not return a value 'void'
+ *
+ * Return: This returns 0 int to main
+ */
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,");
