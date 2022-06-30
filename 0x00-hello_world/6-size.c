6774a4b12d90b0426df91bd15d1b18efe2cc006f
root <emdeefx@gmail.com>
2022-06-30 07:14:17 -0700

    changed syntax code in file 6.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 /**
@@ -6,7 +5,6 @@
  * 
  * Return: returns 0 int value
  */
-
  int main(void)
 {
 	int i;
@@ -15,8 +13,8 @@
 	long long int lln;
 	float f;
 
-	printf("The size of an char: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("the size of a int: %lu byte(s)\n", (unsigned long)sizeof(i);
+	printf("The size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("The size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(ln));
 	printf("The size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(lln));
         printf("The size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
