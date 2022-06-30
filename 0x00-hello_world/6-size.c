4165599031808bb1d70d5ddf468cefea288767d5
root <emdeefx@gmail.com>
2022-06-30 07:17:38 -0700

    changed syntax code in file 6.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@
 	long long int lln;
 	float f;
 
-	printf("The size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("The size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(ln));
-	printf("The size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(lln));
-        printf("The size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(ln));
+	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(lln));
+        printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
