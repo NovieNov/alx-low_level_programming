1aa22d9bd62821e717a28726e3918a74a7e9ec2d
root <emdeefx@gmail.com>
2022-07-01 06:45:19 -0700

    create file 2.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main as the header file
+ *
+ * Return: returns 0 to main.
+ */
+int main(void)
+{	
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
