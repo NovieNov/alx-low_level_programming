84a4c861658258a9858c767acd2492751a355c63
root <emdeefx@gmail.com>
2022-07-01 08:23:14 -0700

    created file 5.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point 
+ *
+ *  Return: Always 0 SUCCESS
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	putchar('\n');
+
+	return (0);
+}
