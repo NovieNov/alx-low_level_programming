0ad13b4b1ac7cfa69546966bdea7a81c88f502de
root <emdeefx@gmail.com>
2022-07-05 05:43:59 -0700

    created a file 0.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.c"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0 SUCCESS
+ */
+int main(void)
+{
+	char *c = "_putchar";
+
+	while(*c)
+	{
+		_putchar(*c);
+		c++;
+	}
+	_putchar('\n');
+
+
+	return(0);
+}
