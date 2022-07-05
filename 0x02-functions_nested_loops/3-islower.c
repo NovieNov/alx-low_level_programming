0ba6f3a94ea5e7320591f62f274c10c29fb9458b
root <emdeefx@gmail.com>
2022-07-05 09:56:14 -0700

    added @c documentation for file 3.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
  * _islower - Check the code
+ *@c: single letter input
  *
- * Return: return either 1 0r 0 
+ * Return: return either 1 0r 0
  */
 int _islower(int c)
 {
@@ -11,7 +12,7 @@ int _islower(int c)
 	{
 		return (1);
 	}
-	else 
+	else
 	{
 		return (0);
 	}
