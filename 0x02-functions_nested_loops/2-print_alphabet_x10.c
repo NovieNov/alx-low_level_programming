2f0d68df605085aa0d3ac54cb09dea57161fb564
root <emdeefx@gmail.com>
2022-07-05 08:39:41 -0700

    created file 2.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Check the code
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0, j;
+
+	while (i < 10)
+	{
+		for (j = 'a'; j <= 'z';)
+		{
+			_putchar(j);
+			j++;
+		}
+	i++;
+	}
+	_putchar('\n');
+}
