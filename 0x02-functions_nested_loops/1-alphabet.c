3314359b2a1e7dd615ff71d6754d30e552d9b6c8
root <emdeefx@gmail.com>
2022-07-05 07:22:25 -0700

    created file 1.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main: Check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z';)
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
