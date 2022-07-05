8c27ad2e3b311317316255a74ab578f06318abd1
root <emdeefx@gmail.com>
2022-07-05 09:19:06 -0700

    created file 3.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - Check the code
+ *
+ * Return: return either 1 0r 0 
+ */
+int _islower(int c)
+{
+	c = 'c';
+
+	if (_islower(c))
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
