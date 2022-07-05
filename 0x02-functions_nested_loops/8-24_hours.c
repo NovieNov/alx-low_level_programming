e7e374dc884da120d35c1a8e48dca9fd19e370d3
root <emdeefx@gmail.com>
2022-07-05 14:38:34 -0700

    created file 8.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Check the code
+ *
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+	int hr, mi;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (mi = 0; mi < 60; mi++)
+		{
+			_putchar(hr / 10 + '0');
+			_putchar(hr % 10 + '0');
+			_putchar(':');
+			_putchar(mi / 10 + '0');
+			_putchar(mi % 10 + '0');
+		}
+	}
+}
