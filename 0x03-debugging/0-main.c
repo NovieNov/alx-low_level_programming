71cf15b6447bebe272bc7a8310922cdfedccd4e4
root <emdeefx@gmail.com>
2022-07-06 06:47:28 -0700

    replaced value i with 0 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- *
+ * main - tests function that prints if integer is positive or negative
+ * Remain: 0
  */
-void positive_or_negative(int i)
+int main(void)
 {
 	int i;
 
 	i = 98;
-	positive_or_negative(i);
+	positive_or_negative(0);
 
 	return (0);
 }
