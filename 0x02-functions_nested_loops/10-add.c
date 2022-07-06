d42abf07dd1d2cab3519c5a3fcd9dad0d2d9793a
root <emdeefx@gmail.com>
2022-07-06 00:49:30 -0700

    created file 10.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - checks the code
+ * @a: a variable with type int
+ * @b: a variable with type int
+ *
+ * Return: returns the sum
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
