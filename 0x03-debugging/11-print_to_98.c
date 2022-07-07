d596ed2431a48e077ae24ec8ab146cb289045295
root <emdeefx@gmail.com>
2022-07-06 21:41:11 -0700

    created file 11.c anew

--- 0x03-debugging/11-print_to_98.c
+++ 0x03-debugging/11-print_to_98.c
@@ -9,9 +9,9 @@
  */
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n > 98)
 	{
-		while (n <= 98)
+		while (n >= 98)
 		{
 			printf("%d", n);
 			if (n != 98)
@@ -21,9 +21,9 @@ void print_to_98(int n)
 			}
 		}
 	}
-	else if (n > 98)
+	else if (n < 98)
 	{
-		while (n >= 98)
+		while (n <= 98)
 		{
 			printf("%d", n);
 			if (n != 98)
