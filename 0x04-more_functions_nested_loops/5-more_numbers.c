7094b30655342f0e04bf949c9915d596ace7b410
root <emdeefx@gmail.com>
2022-07-07 06:09:06 -0700

    created file 5,c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - checks code
+ */
+void more_numbers(void)
+{
+	int i = 0, j;
+
+	while (i < 9)
+	{
+		for (j = 0; j <= 14;)
+		{
+			_putchar(j);
+			j++;
+		}
+		i++;
+		_putchar('\n');
+	}
+}
