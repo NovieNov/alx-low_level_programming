784ec37fddbb2dc31e05c5f2841fd50c4858366e
root <emdeefx@gmail.com>
2022-07-07 06:19:49 -0700

    changed code syntax in file 5.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,7 +7,7 @@ void more_numbers(void)
 {
 	int i = 0, j;
 
-	while (i < 10)
+	while (i <= 9)
 	{
 		for (j = 0; j <= 14;)
 		{
@@ -17,4 +17,5 @@ void more_numbers(void)
 		i++;
 		_putchar('\n');
 	}
+	_putchar('\n');
 }
