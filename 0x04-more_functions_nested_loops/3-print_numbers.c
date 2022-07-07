09efe3bd5677efddfe1bb2bbcb95534a5290f906
root <emdeefx@gmail.com>
2022-07-07 04:07:09 -0700

    included the header for file 0.c in main.h

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_number - check the code
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	if (i = 0; i <= 9)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
