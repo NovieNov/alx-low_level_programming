cfa7a34d96a9891d9e91fda8e9c6f25cd2b75b7f
root <emdeefx@gmail.com>
2022-07-07 05:13:18 -0700

    changed the loop syntax for file 3.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_number - check the code
+ * print_numbers - check the code
  *
  * Return: Always 0
  */
@@ -9,7 +9,7 @@ void print_numbers(void)
 {
 	int i = 0;
 
-	if (i = 0; i <= 9)
+	while (i <= 9)
 	{
 		_putchar(i);
 		i++;
