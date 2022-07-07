656c280b8912ffe836b88485b387a047663a3195
root <emdeefx@gmail.com>
2022-07-07 07:30:11 -0700

    created file 6.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - checks code
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('_');
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
