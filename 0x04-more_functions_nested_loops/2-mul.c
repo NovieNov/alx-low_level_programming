e6909dc60ec05f9e05eb3f71a2890a51f7f4684b
root <emdeefx@gmail.com>
2022-07-07 00:52:25 -0700

    created file 2.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - check for code
+ * @a: a variable with type int
+ * @b: a variable with type int
+ * Return: multiplication of variables
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = (a * b);
+	
+	return (c);
+}
