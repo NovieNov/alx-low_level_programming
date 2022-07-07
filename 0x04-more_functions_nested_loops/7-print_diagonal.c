42885547a99c8bc9a8f3613845ba6899d9492c89
root <emdeefx@gmail.com>
2022-07-07 08:20:47 -0700

    created file 7.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - checks the code
+ */
+void print_diagonal(int n)
+{
+	int m;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		for (m = 1; m <= n; m++)
+		{
+			_putchar(' ');
+			_putchar('\\');
+		}
+		_putchar('\n');
+	}
+}
