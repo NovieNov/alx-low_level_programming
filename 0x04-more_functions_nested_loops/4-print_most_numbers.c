742a89963acc2562f9e21972070c8140d44b0d2f
root <emdeefx@gmail.com>
2022-07-07 05:38:10 -0700

    created file 4.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - checks code
+ *
+ */
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		if (a != 2 && a != 4)
+		{
+			_putchar(a);
+		}
+		a++;
+	}
+	_putchar('\n');
+}
