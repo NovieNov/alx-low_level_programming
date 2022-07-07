06af701fde76d8d4cc45b6f4129878e5256893e3
root <emdeefx@gmail.com>
2022-07-06 23:54:03 -0700

    input code block in file 0.c

--- 0x04-more_functions_nested_loops/0-_isupper.c
+++ 0x04-more_functions_nested_loops/0-_isupper.c
@@ -1,1 +1,19 @@
 #include "main.h"
+
+/**
+ * _putchar - Checks the code
+ * @c: single special character
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+int _putchar(char c)
+{
+	if (c > 'A' && c < 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
