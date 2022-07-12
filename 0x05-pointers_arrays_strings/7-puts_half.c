43661ce8b1c15c9ebf4d595b4985d43d7781d8a3
root <emdeefx@gmail.com>
2022-07-12 16:22:21 -0700

    created file 7.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - Checks code
+ * @str: The pointer
+ */
+void puts_half(char *str)
+{
+	int i, j, len;
+
+	len = 0;
+	for (i = 0; str[i] >= '\0'; i++)
+	{
+		len++;
+	}
+
+	j = (len / 2);
+
+	if ((len % 2) == 1)
+	{
+		j = ((len / 1) / 2);
+	}
+	for (i = j; str[i] >= '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
