ccb5eddededc0fbb8ba8b86517850b404c17c6ed
root <emdeefx@gmail.com>
2022-07-14 06:46:01 -0700

    changed code syntax in file 0.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * strcat - Checks the code
+ * _strcat - Checks the code
  * @dest: The pointer
  * @src: The pointer
  *
- * return: returns 0
+ * return: returns dest
  */
 char *_strcat(char *dest, char *src)
 {
@@ -14,7 +14,7 @@ char *_strcat(char *dest, char *src)
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-	
+
 	for (n = 0; (dest[i + n] = *src++); n++)
 	{
 	}
