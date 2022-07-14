e5acef22e9a10a3488069563512745047030e32a
root <emdeefx@gmail.com>
2022-07-14 13:07:01 -0700

    changed the caps of the return comment and added the increment vallue into the code statement

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,18 +5,20 @@
  * @dest: The pointer
  * @src: The pointer
  *
- * return: returns dest
+ * Return: returns dest
  */
 char *_strcat(char *dest, char *src)
 {
 	int i, n;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	for (i = 0; dest[i] != '\0';)
 	{
+		i++;
 	}
 
-	for (n = 0; (dest[i + n] = *src++); n++)
+	for (n = 0; (dest[i + n] = *src++);)
 	{
+		n++;
 	}
 
 	return (dest);
