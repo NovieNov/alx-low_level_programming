f196f04b0d15d81d247004266ac37c2469995773
root <emdeefx@gmail.com>
2022-07-20 15:54:38 -0700

    created file 5.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+int squareroot(int j, int i)
+/**
+ * _sqrt_recursion - no loop, 1. 2nd function - checks for perfect square
+ * @n: input
+ * Return: Always 0 
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1); 
+	}
+	else
+	{
+		return (squareroot(n, (n + 1) / 2));
+	}
+
+	/**
+	 * squareroot - checks if perfect square
+	 * @n: input
+	 * @i: input
+	 * Return: if square root
+	 */
+	int squareroot(int j, int i)
+	{
+		if (i < 1)
+		{
+			return (-1);
+		}
+		else if (i * i == j)
+		{
+			return (1);
+		}
+		else
+		{
+			return (squareroot(j, i - 1));
+		}
+	}
+}
