da66e1aa8f701c5b2f07dd9ee5d06a6ecada7a5d
root <emdeefx@gmail.com>
2022-07-20 05:37:37 -0700

    created file 0.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Checks the code
+ * @s: The string pointer
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_putchar(*s);
+}
