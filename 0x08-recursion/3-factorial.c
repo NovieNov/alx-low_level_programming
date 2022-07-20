bd3ad5716a4469de75e2d7b4c60a3c1fc15149b0
root <emdeefx@gmail.com>
2022-07-20 08:13:55 -0700

    changed the code syntax to include a base value for the recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,16 +3,19 @@
 /**
  * factorial - Checks the code
  * @n: the value
+ *
+ * Return: returns the value
  */
 int factorial(int n)
 {
-	if (n > 0)	
-	{
-		return (n * factorial(n - 1));
-	}
-
-	else
+	if (n < 0)
 	{
 		return (-1);
 	}
+
+	if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
 }
