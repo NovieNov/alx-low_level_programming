0255e9c8f2287193fc50ca2b4f2a17daab1f67e3
root <emdeefx@gmail.com>
2022-07-20 08:49:19 -0700

    created file 4.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Checks the code
+ * @x: The value input
+ * @y: The value input
+ * Return: returns the the value sum
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x,y - 1));
+}
