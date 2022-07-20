e07b1650f41dd7cb6fe0ec7c9a03ad3b59480bf0
root <emdeefx@gmail.com>
2022-07-20 07:35:19 -0700

    enclosed the return code with a bracket file 2.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,7 +8,6 @@
  */
 int _strlen_recursion(char *s)
 {
-
 	if (*s == '\0')
 	{
 		return (0);
@@ -16,6 +15,6 @@ int _strlen_recursion(char *s)
 
 	else
 	{
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
