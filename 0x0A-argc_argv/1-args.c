e35b177292cdfb55dd13e6a1eb736f4cd5200981
root <emdeefx@gmail.com>
2022-07-22 13:02:25 -0700

    created file 1.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks the code
+ * @argc: counts the commandline argument
+ * @argv: an array holdint commandline arg
+ */
+int main(int argc, __attribute__((unused))char* argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
