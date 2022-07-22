f660412dcb59f3b58f0b9793c1516945ba190c23
root <emdeefx@gmail.com>
2022-07-22 14:27:25 -0700

    created file 2.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,7 +4,7 @@
 /**
  * main - checks the code
  * @argc: counter for the array
- * @argv: the holder for the cl argument 
+ * @argv: the holder for the cl argument
  * Return: Always 0
  */
 int main(int argc, char *argv[])
