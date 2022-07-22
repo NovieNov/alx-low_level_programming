b051d203bdd1b4e507dcbccb984371da91fe5ef1
root <emdeefx@gmail.com>
2022-07-22 13:09:56 -0700

    changed the code syntax

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argc: counts the commandline argument
  * @argv: an array holdint commandline arg
  */
-int main(int argc, __attribute__((unused))char* argv[])
+int main(int argc, __attribute__((unused))char *argv[])
 {
 	printf("%d\n", argc);
 
