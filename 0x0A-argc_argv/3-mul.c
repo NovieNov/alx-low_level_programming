5fcba70becb6b89c89e1a50c72751aa1192cd1fe
root <emdeefx@gmail.com>
2022-07-22 15:28:40 -0700

    created the file 3.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - checks the code
+ * @argc: input
+ * @argbv: input
+ * Return: Always 0
+ */
+int main(int argc, char* argv)
+{
+	int i, mul = 1;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	return (0);
+
+}
