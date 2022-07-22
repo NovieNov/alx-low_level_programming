126f30e01fb66ccf497f4c8c0cf51b2f7895316d
root <emdeefx@gmail.com>
2022-07-22 14:20:58 -0700

    created file 2.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks the code
+ * @argc: counter for the array
+ * @argv: the holder for the cl argument 
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
