a891eba866b9e7012af8d0a6f4b0ad90f4bf8cb8
root <emdeefx@gmail.com>
2022-07-26 15:25:11 -0700

    made changes to the if statement syntax

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,14 @@ char *str_concat(char *s1, char *s2)
 	int i, n, j = 0;
 	char *p;
 
+	if (s1 == NULL && s2 != NULL)
+	{
+		s1 = s2;
+	}	
+	if (s2 == NULL && s1 != NULL)
+	{
+		s2 = s1;
+	}
 	if (s1 == NULL || s2 == NULL)
 	{
 		s1 = s2 = "";
@@ -25,7 +33,7 @@ char *str_concat(char *s1, char *s2)
 		for (n = 0; s2[n] != '\0'; n++)
 		{
 		}
-		
+
 		p = malloc((1 + i + n + 0.5) * sizeof(char));
 
 		if (p == NULL)
