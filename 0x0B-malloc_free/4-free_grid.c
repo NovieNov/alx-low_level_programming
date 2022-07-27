c48341db43c771a6cafe081fab46a0e216174abb
root <emdeefx@gmail.com>
2022-07-27 06:17:05 -0700

    created file 4.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - checks the code
+ * @grid: input
+ * @height: input
+ * Return: A pointer
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid != NULL || height != 0)
+	{
+		for (i = 0; i < height; i++)
+		{
+			free(grid[i]);
+		}
+		free(grid);
+	}
+}
