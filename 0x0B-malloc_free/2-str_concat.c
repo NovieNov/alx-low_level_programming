d6b445c88caf2e1c3ed77cbe9d3f28b9e6dcb8f7
root <emdeefx@gmail.com>
2022-07-27 03:13:50 -0700

    created file 2.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,39 +10,45 @@ char *str_concat(char *s1, char *s2)
 {
 	int i, n, j = 0;
 	char *p;
-	
+
 	if (s1 == NULL && s2 != NULL)
+	{
 		_putchar(*s2);
+		s1++;
+	}
 	if (s1 != NULL && s2 == NULL)
-		_putchar(*s1);
+	{
+		_putchar(*s2);
+		s2++;
+	}
 	if (s1 == NULL || s2 == NULL)
 	{
 		s1 = s2 = "";
 	}
+
+	for (i = 0; s1[i] != '\0'; i++)
 	{
-		for (i = 0; s1[i] != '\0'; i++)
-		{
-		}
-		for (n = 0; s2[n] != '\0'; n++)
-		{
-		}
-		p = malloc((1 + i + n + 0.5) * sizeof(char));
-		if (p == NULL)
-		{
-			return (NULL);
-		}
-		while (*s1)
-		{
-			p[j] = *s1;
-			j++;
-			s1++;
-		}
-		while (*s2)
-		{
-			p[j] = *s2;
-			j++;
-			s2++;
-		}
 	}
+	for (n = 0; s2[n] != '\0'; n++)
+	{
+	}
+	p = malloc((1 + i + n + 0.5) * sizeof(char));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	while (*s1)
+	{
+		p[j] = *s1;
+		j++;
+		s1++;
+	}
+	while (*s2)
+	{
+		p[j] = *s2;
+		j++;
+		s2++;
+	}
+
 	return (p);
 }
