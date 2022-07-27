419a8e7297cfedd2461eb71f888b76ccce103e4e
root <emdeefx@gmail.com>
2022-07-27 03:16:51 -0700

    created file 2.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,16 +11,6 @@ char *str_concat(char *s1, char *s2)
 	int i, n, j = 0;
 	char *p;
 
-	if (s1 == NULL && s2 != NULL)
-	{
-		_putchar(*s2);
-		s1++;
-	}
-	if (s1 != NULL && s2 == NULL)
-	{
-		_putchar(*s2);
-		s2++;
-	}
 	if (s1 == NULL || s2 == NULL)
 	{
 		s1 = s2 = "";
@@ -31,8 +21,9 @@ char *str_concat(char *s1, char *s2)
 	}
 	for (n = 0; s2[n] != '\0'; n++)
 	{
-	}
+	
 	p = malloc((1 + i + n + 0.5) * sizeof(char));
+
 	if (p == NULL)
 	{
 		return (NULL);
