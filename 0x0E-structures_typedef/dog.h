1066126fb6b3840de7f4cc3c74549e34ac9a10a6
root <emdeefx@gmail.com>
2022-08-01 06:07:22 -0700

    Added a documenation in dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_H
+#define dog_H
+
+/**
+ * struct dog - a new type which contains different variables and types
+ */
+
+struct dog 
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+
+#endif
