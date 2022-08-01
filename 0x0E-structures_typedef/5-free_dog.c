e0b8c1919598493f25b18d408217f2e4e6e32078
root <emdeefx@gmail.com>
2022-08-01 10:49:44 -0700

    created file 5.c that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - A function that frees the memory allocated to d
+ * @d: input
+ * Return: return is voided
+ */
+void free_dog(dog_t *d)
+{
+	if (!d)
+		return;
+	free(d);
+}
